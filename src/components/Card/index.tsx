75c3f2f6df3bc3384a8020f7702da1775ed2963c
Vitor Miranda <37985642+vitortcmiranda@users.noreply.github.com>
2021-09-20 08:34:05 -0300

    Changing the font size to rem

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,35 +3,41 @@ export function Card () {
     return (
         <div className={ styles.cardCreator }>
             <div className={styles.card}>
-                <div className={styles.cardShape}>
-                    <div className={styles.cardMonster}>
-                        <img src="/water.svg" alt="" />
-                    </div>
-                    <div className={styles.monsterCircle}>
-                        <img src="/watermonster.png" alt="Monstro" className={styles.monsterImage} />
-                    </div>
-                    <h5 className={styles.monsterTitle}>Serpente Marinha</h5>
-                    <div className={ styles.footer }>
-                        <div className={styles.status}>
-                            <div className={styles.attrs}>
-                                <span>
-                                    <img src="/hp.svg" alt="Vida" />
-                                        10</span>
-                            </div>
-                            <div className={styles.attrs}>
-                                    <img src="/atk.svg" alt="Ataque" className={styles.svg}/>
-                                    <span>10</span>
-                            </div>
-                                
-                            <div className={styles.attrs}>
-                            <span>
-                                    <img src="/energy.svg" alt="Energia" />
-                                    10</span>
-                            </div>
-                            
+            <span className={styles.author}>author: Victor Araujo</span>
+
+                <div className={styles.cardContainer}>
+                    <div className={styles.cardShape}>
+                        <div className={styles.cardMonster}>
+                            <img src="/water.svg" alt="" />
                         </div>
+                        <div className={styles.monsterCircle}>
+                            <img src="/watermonster.png" alt="Monstro" className={styles.monsterImage} />
+                        </div>
+                        <h5 className={styles.monsterTitle}>Serpente Marinha</h5>
+
                     </div>
+                    <div className={ styles.footer }>
+                            <div className={styles.status}>
+                                <div className={styles.attrs}>
+                                    <span>
+                                        <img src="/hp.svg" alt="Vida" />
+                                            10</span>
+                                </div>
+                                <div className={styles.attrs}>
+                                        <img src="/atk.svg" alt="Ataque" className={styles.svg}/>
+                                        <span>10</span>
+                                </div>
+                                    
+                                <div className={styles.attrs}>
+                                <span>
+                                        <img src="/energy.svg" alt="Energia" />
+                                        10</span>
+                                </div>
+                                
+                            </div>
+                        </div>
                 </div>
+                <span className={styles.artist}>art: João das Couves</span>
         </div>
         </div>
     );
